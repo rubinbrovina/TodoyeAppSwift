c59795036f653f59da2d4d86c778693462f37aff
Rubin Brovina <rubin@rubins-mac-mini.local>
2018-08-07 15:50:05 +0200

    Initial Commit

---
+++ Todoye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoye
+//
+//  Created by Rubin Brovina on 07/08/2018.
+//  Copyright © 2018 Rubin Brovina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
