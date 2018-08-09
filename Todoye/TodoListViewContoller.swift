e90acd88ba20d52eb82b3814dc3e0af8deda8af6
Rubin Brovina <rubin@rubins-mac-mini.local>
2018-08-09 12:04:21 +0200

    ToDoList first commit

---
+++ Todoye/TodoListViewContoller.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Todoye
+//
+//  Created by Rubin Brovina on 07/08/2018.
+//  Copyright © 2018 Rubin Brovina. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewContoller: UITableViewController {
+
+    let itemArray = ["rubin","mandi","enio"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+ 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+}
+
